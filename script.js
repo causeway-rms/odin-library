7a3aeadbe2289ff938b63301f51e723aff1a62cf
causeway-rms <118050291+causeway-rms@users.noreply.github.com>
2023-12-02 08:29:30 +0530

    Add the basic idea

---
+++ script.js
@@ -0,0 +1,27 @@
+// Create library
+
+const myLibrary = [];
+
+// Create constructor for Book
+
+function Book(title, author, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.readStatus = readStatus;
+};
+
+// Take user inputs and push it to library
+
+function addBookToLibrary () {
+    let userTitle = prompt('Enter the book title');
+    let userAuthor = prompt('Enter the author name');
+    let userReadStatus = prompt('Enter the read status');
+
+    let userBook = new Book(userTitle, userAuthor, userReadStatus);
+
+    myLibrary.push(userBook);
+}
+
+// Invoke the function
+
+addBookToLibrary();
